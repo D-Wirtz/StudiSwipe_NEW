419432b108ddba5e277339dedc4a007675198dab
Daniel Wirtz <danielwirtz@mail.de>
2023-02-07 01:04:37 +0100

    swipe mechanics

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ let sessions = {}
 
 setInterval(() => {
     for (let x in sessions) {
-        // console.log(sessions[x].expiringTime)
         if (sessions[x].expiringTime < Date.now()) {
             delete sessions[x]
         }
@@ -36,6 +35,10 @@ app.get("/", (req, res) => {
     } else {
         res.render("home")
     }
+})
+
+app.get("/debug", (req, res) => {
+    res.send(accounts.db)
 })
 
 app.get("*", (req, res) => {
@@ -58,23 +61,115 @@ app.post("/login", (req, res) => {
     }
 })
 
+app.post("/logout", (req, res) => {
+    delete sessions[req.cookies["session_token"]]
+    res.redirect("/home")
+})
+
 io.on("connection", (socket) => {
-    log("Socket connect")
+    let currentProfile = null
+
     socket.on("disconnect", () => {
-        log("Socket disconnect")
     })
 
     socket.on("getAccount", (data, res) => {
-        log("getAccount")
-        if (sessions.hasOwnProperty(data)) {
+        if (sessions.hasOwnProperty(data.token)) {
             acc = accounts.get({
-                id: sessions[data].id
+                id: sessions[data.token].id
             })[0]
             res({
                 acc_name: acc.name,
                 acc_bio: acc.bio
             })
         }
+    })
+
+    socket.on("getMatches", (data, res) => {
+        if (sessions.hasOwnProperty(data.token)) {
+            acc = accounts.get({
+                id: sessions[data.token].id
+            })[0]
+            res({
+                acc_matches: acc.matches
+            })
+        }
+    })
+
+    socket.on("getCard", (data) => {
+        if (sessions.hasOwnProperty(data.token)) {
+            acc = accounts.get({
+                id: sessions[data.token].id
+            })[0]
+
+            let a = getCard(acc)
+            currentProfile = a
+            if (currentProfile == null) {
+                socket.emit("newProfile", null)
+                return
+            } else {
+                let data = {
+                    "name": currentProfile.name,
+                    "bio": currentProfile.bio
+                }
+                socket.emit("newCard", data)
+            }
+        }
+    })
+
+    function getCard(acc) {
+        if (acc == null) { return null }
+
+        filter = {
+            "located": acc.dest,
+            "dest": acc.located
+        }
+
+        let accs = accounts.get(filter)
+
+        let profile = null
+        accs.forEach(a => {
+            if (
+                !acc.liked.includes(a.id) && !acc.passed.includes(a.id)
+            ) {
+                profile = a
+                return
+            }
+        });
+
+        return profile
+    }
+
+    socket.on("voteCard", (data) => {
+        if (currentProfile == null) { return null }
+        
+
+        if (sessions.hasOwnProperty(data.token)) {
+            acc = accounts.get({
+                id: sessions[data.token].id
+            })[0]
+            switch (data.action) {
+                case "like":
+                    accounts.db[accounts.db.findIndex((a) => a.id == acc.id)].liked.push(currentProfile.id)
+                    if (accounts.db[accounts.index(currentProfile)].liked.includes(acc.id)) {
+                        accounts.db[accounts.index(currentProfile)].matches.push(acc.id)
+                        let i = accounts.db.findIndex((a) => a.id == acc.id)
+                        accounts.db[i].matches.push(currentProfile.id)
+                        socket.emit("updateMatches", accounts.db[i].matches)
+                    }
+                    currentProfile = null
+                    break;
+
+                case "pass":
+                    accounts.db[accounts.db.findIndex((a) => a.id == acc.id)].passed.push(currentProfile.id)
+                    currentProfile = null
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+
     })
 })
 
